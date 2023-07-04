3d60a89342fde4be7db686bd26076b56a4e134f5
Ayeza <ayezabashir442@gmail.com>
2023-07-04 15:03:29 +0500

    input feilds

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box
+      component="form"
+      noValidate
+      autoComplete="off"
+    >
+      <div className="container_white">
+        <div className="container_padding">
+          <h3 className="container_title">Welcome Back</h3>
+          <p className='container_para'>To keep connected with us, please login with your email adress and password</p>
+          <div className="input_card">
+            <TextField className='input' id="outlined-basic" label="Email" variant="outlined" />
+            <TextField className='input' id="outlined-basic" label="Password" type='password' variant="outlined" />
+          </div>
+        </div>
+      </div>
+    </Box>
+
   );
 }
 
